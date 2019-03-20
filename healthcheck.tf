5a16aa777dc891fc22335bb9667d544722cdf537
Chris Mackubin <chris.mackubin@bluesentryit.com>
2019-03-20 18:02:26 -0400

    add flag to control creation of the healthcheck role

--- healthcheck.tf
+++ healthcheck.tf
@@ -13,11 +13,13 @@ data "aws_iam_policy_document" "BSI-healthcheck-role-policy" {
 }
 
 resource "aws_iam_role" "BSI_healthcheck_role" {
+  count              = "${var.include == "true" && var.enable_msp_healthcheck_role =="true" ? 1 : 0}"
   name               = "BsiHealthCheck"
   assume_role_policy = "${data.aws_iam_policy_document.BSI-healthcheck-role-policy.json}"
 }
 
 resource "aws_iam_role_policy_attachment" "BSI-healthcheck-role-policy-attach" {
+  count      = "${var.include == "true" && var.enable_msp_healthcheck_role =="true" ? 1 : 0}"
   role       = "${aws_iam_role.BSI_healthcheck_role.name}"
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ReadOnlyAccess"
 }
